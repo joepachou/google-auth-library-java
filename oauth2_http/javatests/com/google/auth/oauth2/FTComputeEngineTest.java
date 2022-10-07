16ff6687ba0ca14e02ac0ee4bd4a3de789881612
Joe Chou <poanc@google.com>
2022-10-07 10:49:49 -0700

    test github gce environment

---
+++ oauth2_http/javatests/com/google/auth/oauth2/FTComputeEngineTest.java
@@ -0,0 +1,65 @@
+package com.google.auth.oauth2;
+
+
+import com.google.auth.oauth2.ComputeEngineCredentials;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import com.google.auth.http.HttpTransportFactory;
+import com.google.auth.oauth2.GoogleCredentialsTest.MockHttpTransportFactory;
+import com.google.api.client.http.HttpTransport;
+import com.google.auth.oauth2.DefaultCredentialsProvider;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public final class FTComputeEngineTest {
+
+//    static class MockMetadataServerTransportFactory implements HttpTransportFactory {
+//
+//        MockMetadataServerTransport transport = new MockMetadataServerTransport();
+//
+//        @Override
+//        public HttpTransport create() {
+//            return transport;
+//        }
+//    }
+
+    @Test
+    public void CheckGCEEnvironment() throws Exception{}
+
+    @Test
+    public void refreshTest() throws Exception {
+
+//        String accessToken = "1/MkSJoj1xsli0AccessToken_NKPY2";
+//        String accountEmail = "mail@mail.com";
+//
+//        MockMetadataServerTransportFactory transportFactory = new MockMetadataServerTransportFactory();
+//        transportFactory.transport.setAccessToken(accessToken);
+//        transportFactory.transport.setServiceAccountEmail(accountEmail);
+
+        ComputeEngineCredentials credentials = ComputeEngineCredentials.create();
+
+        credentials.refresh();
+
+        assertNotNull(credentials.getAccessToken());
+        assertNotNull(credentials.getAccount());
+
+        // TODO: test scope
+//        assertEquals(credentials.getScopes(), "foo");
+    }
+
+    @Test
+    public void defaultTest() throws Exception {
+    }
+
+    @Test
+    public void idTokenFromMetadataTest() throws Exception {
+
+    }
+
+    @Test
+    public void fetchIdTokenTest() throws Exception {}
+
+}
